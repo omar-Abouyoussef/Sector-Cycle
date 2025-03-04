db0e8c03564c0dcdb933a91e2c2c58dcf6c83de3
Omar <101734132+omar-Abouyoussef@users.noreply.github.com>
2025-03-04 19:04:56 +0200

    Update Tear_sheet.py

--- pages/Tear_sheet.py
+++ pages/Tear_sheet.py
@@ -2,9 +2,9 @@ import pyfolio as pf
 import streamlit as st
 import pandas as pd
 
-st.write(pf.create_full_tear_sheet(
+pf.create_full_tear_sheet(
     returns=st.session_state.returns,
     transactions=st.session_state.transactions,
     live_start_date='2022-01-01',
     estimate_intraday=False,
-    round_trips=False))
+    round_trips=False)
