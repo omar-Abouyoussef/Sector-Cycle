d16441a3112748d493d3037fe345f183bfa9b26b
Omar <101734132+omar-Abouyoussef@users.noreply.github.com>
2025-03-04 21:25:40 +0200

    Update Backtesting.py

--- Backtesting.py
+++ Backtesting.py
@@ -255,9 +255,9 @@ fig.update_layout(
 st.plotly_chart(fig)
 
 
-st.write(pf.create_full_tear_sheet(
+pf.create_returns_tear_sheet(
     returns=st.session_state.returns,
     transactions=st.session_state.transactions,
     live_start_date='2022-01-01',
     estimate_intraday=False,
-    round_trips=False))
+    round_trips=False)
